a7f071b2cd50b3821197c939526e6c6ebb9d6768
Sergey <seryi.zadorin@gmail.com>
2023-12-02 16:54:01 +0400

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')({
+    logger: true,
+});
+
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'world' })
+});
+
+fastify.listen({ port: 8080 }, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+});
