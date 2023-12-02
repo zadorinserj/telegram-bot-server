475b950a67aa5e1e8c5f6597e24937601ecc7b0f
Sergey <seryi.zadorin@gmail.com>
2023-12-02 18:21:18 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ fastify.get('/', function (request, reply) {
     reply.send({ hello: 'world' })
 });
 
-fastify.get('/home', function (request, reply) {
-    reply.send({ hello: 'world' })
+fastify.get('/home/', function (request, reply) {
+    reply.status(200).send({ hello: 'world' })
 });
 
 fastify.listen({ host: '0.0.0.0', port: 8080 }, function (err, address) {
